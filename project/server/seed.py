ae7e0111605b40c2f3d94e9d14537acd5ee7306b
Chrispus <skilochris@gmail.com>
2024-02-06 15:35:53 +0300

    added seed data

---
+++ project/server/seed.py
@@ -0,0 +1,56 @@
+from random import choice as rc
+
+from faker import Faker
+
+from app import app
+from models import db, Employee, Payroll, Attendance
+
+
+fake = Faker()
+
+with app.app_context():
+
+    Employee.query.delete()
+    Payroll.query.delete()
+    Attendance.query.delete()
+
+    employees = []
+    for _ in range(10):  
+        employee = Employee(
+            name=fake.name(),
+            department=fake.word(),
+            position=rc(['Junior', 'Senior']),
+        )
+        db.session.add(employee)
+        employees.append(employee)
+
+    db.session.commit()
+
+    # Create associated payroll records for each employee
+    for employee in employees:
+        payroll = Payroll(
+            month=fake.random_int(min=1, max=12),
+            year=fake.random_int(min=2022, max=2023),
+            total_hours_worked=fake.random_int(min=1, max=40),
+            hourly_rate=fake.random_int(min=5, max=10),
+            leave_deduction_rate=fake.random_int(min=3, max=5),
+            bonus_rate=fake.random_int(min=0, max=2),
+            tax_deduction_rate=fake.random_int(min=6, max=8),
+            calculated_salary=fake.random_int(min=2000, max=5000),
+            employee=employee,
+        )
+        db.session.add(payroll)
+
+    db.session.commit()
+
+    # Create associated attendance records for each employee
+    for employee in employees:
+        attendance = Attendance(
+            date=fake.date_this_year(),
+            hours_worked=fake.random_int(min=0, max=9),
+            leave_taken=fake.boolean(),
+            employee=employee,
+        )
+        db.session.add(attendance)
+
+    db.session.commit()
