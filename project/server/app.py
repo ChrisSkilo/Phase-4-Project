ae403c008d56b8f60776be3e2bea6598af3c628a
Chrispus <skilochris@gmail.com>
2024-02-06 15:20:41 +0300

    created tables

--- project/server/app.py
+++ project/server/app.py
@@ -2,3 +2,33 @@ from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
 from flask_restful import Api,Resource
 from werkzeug.exceptions import NotFound
+
+
+from models import db, User, Employee, Payroll
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+api = Api(app)
+
+@app.errorhandler(NotFound)
+def handle_not_found(e):
+    response= make_response("NotFound: The requested resource not found", 404)
+    return response
+
+@app.route('/')
+def home():
+    return "PAYROLL SYSTEM"
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
